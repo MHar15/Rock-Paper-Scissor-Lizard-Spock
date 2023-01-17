163888877bfae3cb8df5128f97e5112c1164a7f6
Matt Hargreaves <MatthewHargreaves@outlook.com>
2023-01-17 20:24:57 +0000

    123456

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userchoice
+
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-userChoice = e.target.id
+  userChoiceDisplay = e.target.id
+  userChoiceDisplay.innerHTML = user-choice
+  generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+   const randomNumber = Math.random() * possibleChoices.length
+   console.log(randomNumber)
+}
